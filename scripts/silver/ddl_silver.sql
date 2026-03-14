2bfe939d1a7d523e9336d86a6545a54f48d8a159
Sebastian Delgado <sebasplay417@gmail.com>
2026-03-13 23:23:09 -0600

    add new stored procedure to load data into silver schema and update crm_prd_info table structure

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -33,6 +33,7 @@ CREATE TABLE silver.crm_cust_info (
 
 CREATE TABLE silver.crm_prd_info (
     prd_id          INT,
+    cat_id          VARCHAR(50),
     prd_key         VARCHAR(50),
     prd_nm          VARCHAR(50),
     prd_cost        INT,
@@ -46,9 +47,9 @@ CREATE TABLE silver.crm_sales_details (
     sls_ord_num     VARCHAR(50),
     sls_prd_key     VARCHAR(50),
     sls_cust_id     INT,
-    sls_order_dt    INT,
-    sls_ship_dt     INT,
-    sls_due_dt      INT,
+    sls_order_dt    TIMESTAMP,
+    sls_ship_dt     TIMESTAMP,
+    sls_due_dt      TIMESTAMP,
     sls_sales       INT,
     sls_quantity    INT,
     sls_price       INT,
