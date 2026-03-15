b1e16eca95583e75d0b74d06b05927f46ba646b7
Sebastian Delgado <sebasplay417@gmail.com>
2026-03-15 12:31:42 -0600

    add: create gold.fact_sales view and ensure clean slate for gold.dim_customers and gold.dim_products

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -13,12 +13,16 @@ Usage:
     - These views can be queried directly for analytics and reporting.
 ===============================================================================
 */
+-- =============================================================================
+-- Drop existing views if they exist to ensure a clean slate for creation
+-- =============================================================================
+DROP VIEW IF EXISTS gold.fact_sales;
+DROP VIEW IF EXISTS gold.dim_customers;
+DROP VIEW IF EXISTS gold.dim_products;
 
 -- =============================================================================
 -- Create Dimension: gold.dim_customers
 -- =============================================================================
-DROP VIEW IF EXISTS gold.dim_customers;
-
 CREATE VIEW gold.dim_customers AS
 SELECT
     ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key, -- Surrogate key
@@ -44,8 +48,6 @@ LEFT JOIN silver.erp_loc_a101 la
 -- =============================================================================
 -- Create Dimension: gold.dim_products
 -- =============================================================================
-DROP VIEW IF EXISTS gold.dim_products;
-
 CREATE VIEW gold.dim_products AS
 SELECT
     ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, -- Surrogate key
@@ -63,3 +65,23 @@ FROM silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
     ON pn.cat_id = pc.id
 WHERE pn.prd_end_dt IS NULL; -- Filter out all historical data
+
+-- =============================================================================
+-- Create Fact Table: gold.fact_sales
+-- =============================================================================
+CREATE VIEW gold.fact_sales AS
+SELECT
+    sd.sls_ord_num  AS order_number,
+    pr.product_key  AS product_key,
+    cu.customer_key AS customer_key,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt  AS shipping_date,
+    sd.sls_due_dt   AS due_date,
+    sd.sls_sales    AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price    AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+    ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+    ON sd.sls_cust_id = cu.customer_id;
